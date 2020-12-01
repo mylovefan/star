b4ded2267a032b4f0412b66ae80f0e3db07f06e6
lu_jiakang <lu88888>
2020-12-01 21:46:05 +0800

    新增/查询明星标签信息

--- star-app/src/main/java/com/star/module/operation/service/ITagsService.java
+++ star-app/src/main/java/com/star/module/operation/service/ITagsService.java
@@ -2,6 +2,9 @@ package com.star.module.operation.service;
 
 import com.star.module.operation.entity.Tags;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.star.module.user.vo.TagsVo;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ITagsService extends IService<Tags> {
 
+    List<TagsVo> tagsList();
+
+    void addTags(String name);
 }
--- star-app/src/main/java/com/star/module/operation/service/impl/TagsServiceImpl.java
+++ star-app/src/main/java/com/star/module/operation/service/impl/TagsServiceImpl.java
@@ -1,10 +1,23 @@
 package com.star.module.operation.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.star.common.CommonConstants;
+import com.star.common.ErrorCodeEnum;
+import com.star.common.ServiceException;
 import com.star.module.operation.dao.TagsMapper;
 import com.star.module.operation.entity.Tags;
 import com.star.module.operation.service.ITagsService;
+import com.star.module.operation.util.ListUtils;
+import com.star.module.user.vo.TagsVo;
+import com.star.util.SnowflakeId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +30,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class TagsServiceImpl extends ServiceImpl<TagsMapper, Tags> implements ITagsService {
 
+    @Autowired
+    private TagsMapper tagsMapper;
+    @Autowired
+    private ListUtils listUtils;
+
+    @Override
+    public List<TagsVo> tagsList() {
+        QueryWrapper<Tags> wrapper = new QueryWrapper<>();
+        List<Tags> tagsList = tagsMapper.selectList(wrapper);
+        if(tagsList.size()>0){
+            List<TagsVo> tagsVoList = new ArrayList<>();
+            listUtils.copyList(tagsList, tagsVoList, TagsVo.class);
+            return tagsVoList;
+        }
+        return null;
+    }
+
+    @Override
+    public void addTags(String name) {
+        QueryWrapper<Tags> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(Tags::getName, name);
+        Tags tags = tagsMapper.selectOne(queryWrapper);
+        if(tags != null){
+            throw new ServiceException(ErrorCodeEnum.ERROR_200001.getCode(),"同名标签已存在");
+        }
+        Tags tagsNew = new Tags();
+        tagsNew.setName(name);
+        tagsNew.setId(SnowflakeId.getInstance().nextId());
+        LocalDateTime localDateTimeOfNow = LocalDateTime.now(ZoneId.of(CommonConstants.ZONEID_SHANGHAI));
+        tagsNew.setAddTime(localDateTimeOfNow);
+        tagsMapper.insert(tagsNew);
+    }
 }
