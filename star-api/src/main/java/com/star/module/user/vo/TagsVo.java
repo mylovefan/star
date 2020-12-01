6140734748c53c543d78dc172f55d3677e6d5c82
lu_jiakang <lu88888>
2020-12-01 21:50:15 +0800

    新增/查询明星标签信息

---
+++ star-api/src/main/java/com/star/module/user/vo/TagsVo.java
@@ -0,0 +1,38 @@
+package com.star.module.user.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author zhangrc <1538618608@qq.com>
+ * @since 2020-12-01
+ */
+@Data
+public class TagsVo {
+
+    @ApiModelProperty(value = "id")
+    private Long id;
+
+    @ApiModelProperty(value = "名称")
+    private String name;
+
+    @ApiModelProperty(value = "新增时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime addTime;
+
+
+}
