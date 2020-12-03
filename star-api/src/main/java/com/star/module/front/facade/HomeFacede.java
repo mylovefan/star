ba22eb1244c5fd83358ba767e189f5de6deb3cf9
ljk <ljk@gratong.com>
2020-12-03 19:14:54 +0800

    首页排行榜-周榜

--- star-api/src/main/java/com/star/module/front/facade/HomeFacede.java
+++ star-api/src/main/java/com/star/module/front/facade/HomeFacede.java
@@ -1,15 +1,10 @@
 package com.star.module.front.facade;
 
 import com.github.pagehelper.PageSerializable;
-import com.star.commen.dto.PageDTO;
-import com.star.module.front.vo.WeekRankVo;
-import com.star.module.operation.dto.StarPageDto;
-import com.star.module.operation.vo.StartVo;
+import com.star.module.operation.vo.HitListVo;
+import com.star.module.front.dto.RankDto;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("home/")
 public interface HomeFacede {
 
-    @ApiOperation("周榜排名列表")
-    @GetMapping("weekRank/list")
-    PageSerializable<WeekRankVo> pageListWeekRank(@RequestBody PageDTO pageDTO);
+    @ApiOperation("周榜月榜总榜排名")
+    @PostMapping("weekRank/list")
+    PageSerializable<HitListVo> pageListRank(@RequestBody RankDto rankDto);
 
 }
--- star-app/src/main/java/com/star/module/front/controller/HomeController.java
+++ star-app/src/main/java/com/star/module/front/controller/HomeController.java
@@ -2,11 +2,14 @@ package com.star.module.front.controller;
 
 import com.github.pagehelper.PageSerializable;
 import com.star.commen.dto.PageDTO;
+import com.star.module.front.dto.RankDto;
 import com.star.module.front.facade.HomeFacede;
 import com.star.module.front.service.IHitListService;
 import com.star.module.front.vo.WeekRankVo;
+import com.star.module.operation.vo.HitListVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,7 +18,7 @@ public class HomeController implements HomeFacede {
     private IHitListService iHitListService;
 
     @Override
-    public PageSerializable<WeekRankVo> pageListWeekRank(PageDTO pageDTO) {
-        return null;
+    public PageSerializable<HitListVo> pageListRank(@RequestBody RankDto rankDto) {
+        return iHitListService.pageListRank(rankDto);
     }
 }
